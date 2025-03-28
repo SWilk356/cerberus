13105abbe5513269935f2962a9ad062ded5c63b1
sarah.wilkinson_omni <sarah.wilkinson@omnifederal.com>
2025-03-28 17:00:42 -0500

    fix types in rule

--- packages/panda-shorthand/src/index.ts
+++ packages/panda-shorthand/src/index.ts
@@ -1,3 +1,91 @@
-export function pandaShorthand() {
-  return 'panda-shorthand'
+import { preset } from '@pandacss/preset-base'
+import type { PropertyConfig } from '@pandacss/types'
+import { AST_NODE_TYPES, TSESLint, TSESTree } from '@typescript-eslint/utils'
+import { RuleFixer } from '@typescript-eslint/utils/ts-eslint'
+
+//TODO: make sure is eslint8 compatible - flat config
+module.exports = {
+  meta: {
+    type: 'suggestion',
+    docs: {
+      description: 'Enforce Panda CSS shorthand property names',
+      category: 'Best Practices',
+    },
+    messages: {
+      useShorthand:
+        "Use Panda CSS shorthand '{{ shorthand }}' instead of '{{ original }}'.",
+    },
+    fixable: 'code',
+    schema: [],
+  },
+  create: function (context: {
+    report: (arg0: {
+      node: TSESTree.Identifier
+      messageId: string
+      data: { shorthand: string; original: string }
+      fix(fixer: RuleFixer): TSESLint.RuleFix
+    }) => void
+  }) {
+    // Mapping of standard CSS properties to Panda CSS shorthands
+    const propertyMap: { [key: string]: string | string[] } = {}
+    for (const cssProperty of Object.entries(preset.utilities)) {
+      const propertyName: string = cssProperty[0]
+      const propertyObj: PropertyConfig | undefined = cssProperty[1]
+      if (propertyObj?.shorthand) {
+        // there is a rare instance where the shorthand is the same as the property name, so do not count it as a shorthand
+        if (propertyName !== propertyObj.shorthand) {
+          propertyMap[propertyName] = propertyObj.shorthand
+        }
+      }
+    }
+
+    return {
+      CallExpression(node: TSESTree.CallExpression): void {
+        const patterns: string[] = [
+          ...Object.entries(preset.patterns).map(([key]) => key),
+          'css',
+        ]
+
+        // only flag violations for panda pattern functions
+        if (
+          node.callee.type === AST_NODE_TYPES.Identifier &&
+          patterns.includes(node.callee.name) &&
+          node.arguments.length > 0
+        ) {
+          const arg: TSESTree.CallExpressionArgument = node.arguments[0]
+
+          if (arg.type === AST_NODE_TYPES.ObjectExpression) {
+            for (const property of arg.properties) {
+              if (
+                property.type === AST_NODE_TYPES.Property &&
+                property.key.type === AST_NODE_TYPES.Identifier &&
+                propertyMap[property.key.name]
+              ) {
+                const shorthand = propertyMap[property.key.name]
+                const shorthandString = Array.isArray(shorthand)
+                  ? shorthand.join(' or ') // there may be multiple shorthands
+                  : shorthand
+
+                context.report({
+                  node: property.key,
+                  messageId: 'useShorthand',
+                  data: {
+                    shorthand: shorthandString,
+                    original: property.key.name,
+                  },
+                  fix(fixer: RuleFixer) {
+                    const replacement = Array.isArray(shorthand)
+                      ? shorthand[0]
+                      : shorthand
+
+                    return fixer.replaceText(property.key, replacement)
+                  },
+                })
+              }
+            }
+          }
+        }
+      },
+    }
+  },
 }
